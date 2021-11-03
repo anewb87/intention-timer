672b49314cd534005731ff2dbcfbc418269d96fc
Grace Gardner <graceirisgardner@gmail.com>
2021-11-02 18:18:55 -0600

    Add error functionality to start activity button

--- main.js
+++ main.js
@@ -1,17 +1,65 @@
 var studyBtn = document.querySelector(".study-button");
 var meditateBtn = document.querySelector(".meditate-button");
 var exerciseBtn = document.querySelector(".exercise-button");
+var startActivityBtn = document.querySelector(".start-activity-button");
 var studyImg = document.querySelector(".study-img");
 var studyImgActive = document.querySelector(".study-img-active");
 var meditateImg = document.querySelector(".meditate-img");
 var meditateImgActive = document.querySelector(".meditate-img-active");
-var exerciseImg = document.querySelector(".exercise-img")
-var exerciseImgActive = document.querySelector(".exercise-img-active")
-
+var exerciseImg = document.querySelector(".exercise-img");
+var exerciseImgActive = document.querySelector(".exercise-img-active");
+var activityInput = document.querySelector(".accomplish-input");
+var minutesInput = document.getElementById("minutes-input");
+var secondsInput = document.getElementById("seconds-input");
+var activityError = document.querySelector(".description-error");
+var minutesError = document.querySelector(".minutes-error");
+var secondsError = document.querySelector(".seconds-error");
 
 studyBtn.addEventListener("click", activateStudyColor);
 meditateBtn.addEventListener("click", activateMeditateColor);
 exerciseBtn.addEventListener("click", activateExerciseColor);
+startActivityBtn.addEventListener("click", startActivity);
+
+function startActivity() {
+  throwError()
+}
+// change colors
+// add in error image + text
+//
+function addErrorStyling(input) {
+  input.classList.add("error");
+}
+
+function removeErrorStyling(input) {
+  input.classList.remove("error");
+}
+
+function throwError() {
+  if (!activityInput.value) {
+    addErrorStyling(activityInput);
+    show(activityError);
+  } if (!minutesInput.value) {
+    addErrorStyling(minutesInput);
+    show(minutesError);
+  } if (!secondsInput.value) {
+    addErrorStyling(secondsInput);
+    show(secondsError);
+  }
+  removeError();
+};
+
+function removeError() {
+  if (activityInput.value) {
+    removeErrorStyling(activityInput);
+    hide(activityError);
+  } if (minutesInput.value) {
+    removeErrorStyling(minutesInput);
+    hide(minutesError);
+  } if (secondsInput.value) {
+    removeErrorStyling(secondsInput);
+    hide(secondsError);
+  }
+}
 
 function show(element) {
   element.classList.remove("hidden")
